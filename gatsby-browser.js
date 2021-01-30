ad2f7b3825b8e6c88e8ab50cc1d49e14eead22ea
r951236958 <r951236958@gmail.com>
2021-01-31 03:12:36 +0800

    修改url

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,11 +9,11 @@ import React from "react"
 import { IdentityContextProvider } from "react-netlify-identity-widget"
 
 export const wrapRootElement = ({ element }) => {
-  const url = process.env.GATSBY_NETLIFY_IDENTITY_URL
+  const url = "https://gatsby-with-netlify-identity.netlify.app/"
   // process.env.GATSBY_NETLIFY_IDENTITY_URL || "url here for running locally" // should look something like "https://foo.netlify.com"
   if (!url)
     throw new Error(
-      "process.env.GATSBY_NETLIFY_IDENTITY_URL is blank2, which means you probably forgot to set it in your Netlify environment variables"
+      "https://gatsby-with-netlify-identity.netlify.app/ is blank2, which means you probably forgot to set it in your Netlify environment variables"
     )
 
   return <IdentityContextProvider url={url}>{element}</IdentityContextProvider>
